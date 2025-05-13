3c3fd8ada613092f78b91b2949f93c1efcb628fe
Kai Indigo Wolf <kaiindigowolf@pantheon.io>
2025-05-13 13:38:48 -0500

    bug fix empty argument

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import vercel from '@astrojs/vercel/serverless'; // ✅ add this
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(), // ✅ add this
+  adapter: vercel({}), // ✅ add this
 });
